94ad743dfa647dc41c12633289ab3b17707a9309
NewsGram <mhmdpapy47@gmail.com>
2017-05-05 01:00:58 +0430

    Revert "News_Gram"
    This reverts commit 6a9f6d16dad1c19707182863d92b867b14acd686.

--- TMessagesProj/src/main/java/org/telegram/setting/TabListener.java
+++
@@ -1,9 +0,0 @@
-package org.telegram.setting;
-
-public interface TabListener {
-	public void onTabSelected(MaterialTab tab);
-	
-	public void onTabReselected(MaterialTab tab);
-	
-	public void onTabUnselected(MaterialTab tab);
-}
